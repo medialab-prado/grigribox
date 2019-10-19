ee789dab9842dd372761884186d9c0594e4d942b
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2019-10-19 19:44:09 +0200

    Initial commit

---
+++ parts/body.php
@@ -0,0 +1,99 @@
+<form action="" method="post">
+<input type="hidden" name="p" value="<?php echo fm_enc(FM_PATH) ?>">
+<input type="hidden" name="group" value="1">
+<table><tr>
+<th style="width:3%"><label><input type="checkbox" title="Invert selection" onclick="checkbox_toggle()"></label></th>
+<th>Name</th><th style="width:10%">Size</th>
+<th style="width:12%">Modified</th>
+<th style="width:13%"></th></tr>
+<?php
+// link to parent folder
+if ($parent !== false) {
+    ?>
+<tr><td></td><td colspan="4"><a href="?p=<?php echo urlencode($parent) ?>"><i class="icon-arrow_up"></i> ..</a></td></tr>
+<?php
+}
+foreach ($folders as $f) {
+    $is_link = is_link($path . '/' . $f);
+    $img = $is_link ? 'icon-link_folder' : 'icon-folder';
+    $modif = date(FM_DATETIME_FORMAT, filemtime($path . '/' . $f));
+    $perms = substr(decoct(fileperms($path . '/' . $f)), -4);
+    if (function_exists('posix_getpwuid') && function_exists('posix_getgrgid')) {
+        $owner = posix_getpwuid(fileowner($path . '/' . $f));
+        $group = posix_getgrgid(filegroup($path . '/' . $f));
+    } else {
+        $owner = array('name' => '?');
+        $group = array('name' => '?');
+    }
+    ?>
+<tr>
+<td><label><input type="checkbox" name="file[]" value="<?php echo fm_enc($f) ?>"></label></td>
+<td><div class="filename"><a href="?p=<?php echo urlencode(trim(FM_PATH . '/' . $f, '/')) ?>"><i class="<?php echo $img ?>"></i> <?php echo fm_enc(fm_convert_win($f)) ?></a><?php echo ($is_link ? ' &rarr; <i>' . fm_enc(readlink($path . '/' . $f)) . '</i>' : '') ?></div></td>
+<td>Folder</td><td><?php echo $modif ?></td>
+<td>
+<a title="Delete" href="?p=<?php echo urlencode(FM_PATH) ?>&amp;del=<?php echo urlencode($f) ?>" onclick="return confirm('Delete folder?');"><i class="icon-cross"></i></a>
+<a title="Rename" href="#" onclick="rename('<?php echo fm_enc(FM_PATH) ?>', '<?php echo fm_enc($f) ?>');return false;"><i class="icon-rename"></i></a>
+<a title="Copy to..." href="?p=&amp;copy=<?php echo urlencode(trim(FM_PATH . '/' . $f, '/')) ?>"><i class="icon-copy"></i></a>
+<a title="Direct link" href="<?php echo fm_enc(FM_ROOT_URL . (FM_PATH != '' ? '/' . FM_PATH : '') . '/' . $f . '/') ?>" target="_blank"><i class="icon-chain"></i></a>
+</td></tr>
+    <?php
+    flush();
+}
+
+foreach ($files as $f) {
+    $is_link = is_link($path . '/' . $f);
+    $img = $is_link ? 'icon-link_file' : fm_get_file_icon_class($path . '/' . $f);
+    $modif = date(FM_DATETIME_FORMAT, filemtime($path . '/' . $f));
+    $filesize_raw = filesize($path . '/' . $f);
+    $filesize = fm_get_filesize($filesize_raw);
+    $filelink = '?p=' . urlencode(FM_PATH) . '&view=' . urlencode($f);
+    $all_files_size += $filesize_raw;
+    $perms = substr(decoct(fileperms($path . '/' . $f)), -4);
+    if (function_exists('posix_getpwuid') && function_exists('posix_getgrgid')) {
+        $owner = posix_getpwuid(fileowner($path . '/' . $f));
+        $group = posix_getgrgid(filegroup($path . '/' . $f));
+    } else {
+        $owner = array('name' => '?');
+        $group = array('name' => '?');
+    }
+    ?>
+<tr>
+<td><label><input type="checkbox" name="file[]" value="<?php echo fm_enc($f) ?>"></label></td>
+<td><div class="filename"><a href="<?php echo fm_enc($filelink) ?>" title="File info"><i class="<?php echo $img ?>"></i> <?php echo fm_enc(fm_convert_win($f)) ?></a><?php echo ($is_link ? ' &rarr; <i>' . fm_enc(readlink($path . '/' . $f)) . '</i>' : '') ?></div></td>
+<td><span class="gray" title="<?php printf('%s bytes', $filesize_raw) ?>"><?php echo $filesize ?></span></td>
+<td><?php echo $modif ?></td>
+<td>
+<a title="Delete" href="?p=<?php echo urlencode(FM_PATH) ?>&amp;del=<?php echo urlencode($f) ?>" onclick="return confirm('Delete file?');"><i class="icon-cross"></i></a>
+<a title="Rename" href="#" onclick="rename('<?php echo fm_enc(FM_PATH) ?>', '<?php echo fm_enc($f) ?>');return false;"><i class="icon-rename"></i></a>
+<a title="Copy to..." href="?p=<?php echo urlencode(FM_PATH) ?>&amp;copy=<?php echo urlencode(trim(FM_PATH . '/' . $f, '/')) ?>"><i class="icon-copy"></i></a>
+<a title="Direct link" href="<?php echo fm_enc(FM_ROOT_URL . (FM_PATH != '' ? '/' . FM_PATH : '') . '/' . $f) ?>" target="_blank"><i class="icon-chain"></i></a>
+<a title="Download" href="?p=<?php echo urlencode(FM_PATH) ?>&amp;dl=<?php echo urlencode($f) ?>"><i class="icon-download"></i></a>
+</td></tr>
+    <?php
+    flush();
+}
+
+if (empty($folders) && empty($files)) {
+    ?>
+<tr><td></td><td colspan="<?php echo !FM_IS_WIN ? '6' : '4' ?>"><em>Folder is empty</em></td></tr>
+<?php
+} else {
+    ?>
+<tr><td class="gray"></td><td class="gray" colspan="<?php echo !FM_IS_WIN ? '6' : '4' ?>">
+Full size: <span title="<?php printf('%s bytes', $all_files_size) ?>"><?php echo fm_get_filesize($all_files_size) ?></span>,
+files: <?php echo $num_files ?>,
+folders: <?php echo $num_folders ?>
+</td></tr>
+<?php
+}
+?>
+</table>
+<p class="path"><a href="#" onclick="select_all();return false;"><i class="icon-checkbox"></i> Select all</a> &nbsp;
+<a href="#" onclick="unselect_all();return false;"><i class="icon-checkbox_uncheck"></i> Unselect all</a> &nbsp;
+<a href="#" onclick="invert_all();return false;"><i class="icon-checkbox_invert"></i> Invert selection</a></p>
+<p><input type="submit" name="delete" value="Delete" onclick="return confirm('Delete selected files and folders?')">
+<input type="submit" name="zip" value="Pack" onclick="return confirm('Create archive?')">
+<input type="submit" name="copy" value="Copy"></p>
+</form>
+
+<?php
---
+++ parts/header.php
@@ -0,0 +1,75 @@
+<?php
+$sprites_ver = '20160315';
+header("Content-Type: text/html; charset=utf-8");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+header("Pragma: no-cache");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Grigri Box</title>
+<style>
+html,body,div,span,p,pre,a,code,em,img,small,strong,ol,ul,li,form,label,table,tr,th,td{margin:0;padding:0;vertical-align:baseline;outline:none;font-size:100%;background:transparent;border:none;text-decoration:none}
+html{overflow-y:scroll}body{padding:0;font:13px/16px Tahoma,Arial,sans-serif;color:#222;}
+input,select,textarea,button{font-size:inherit;font-family:inherit}
+a{color:#296ea3;text-decoration:none}a:hover{color:#b00}img{vertical-align:middle;border:none}
+a img{border:none}span.gray{color:#777}small{font-size:11px;color:#999}p{margin-bottom:10px}
+ul{margin-left:2em;margin-bottom:10px}ul{list-style-type:none;margin-left:0}ul li{padding:3px 0}
+table{border-collapse:collapse;border-spacing:0;margin-bottom:10px;width:100%}
+th,td{padding:4px 7px;text-align:left;vertical-align:top;border:1px solid #ddd;background:#fff;white-space:nowrap}
+th,td.gray{background-color:#eee}td.gray span{color:#222}
+tr:hover td{background-color:#f5f5f5}tr:hover td.gray{background-color:#eee}
+code,pre{display:block;margin-bottom:10px;font:13px/16px Consolas,'Courier New',Courier,monospace;border:1px dashed #ccc;padding:5px;overflow:auto}
+pre.with-hljs{padding:0}
+pre.with-hljs code{margin:0;border:0;overflow:visible}
+code.maxheight,pre.maxheight{max-height:512px}input[type="checkbox"]{margin:0;padding:0}
+#wrapper{max-width:1000px;min-width:400px;margin:10px auto}
+.path{padding:4px 7px;border:1px solid #ddd;background-color:#fff;margin-bottom:10px}
+.right{text-align:right}.center{text-align:center}.float-right{float:right}
+.message{padding:4px 7px;border:1px solid #ddd;background-color:#fff}
+.message.ok{border-color:green;color:green}
+.message.error{border-color:red;color:red}
+.message.alert{border-color:orange;color:orange}
+.btn{border:0;background:none;padding:0;margin:0;font-weight:bold;color:#296ea3;cursor:pointer}.btn:hover{color:#b00}
+.preview-img{max-width:100%;background:url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAAAKklEQVR42mL5//8/Azbw+PFjrOJMDCSCUQ3EABZc4S0rKzsaSvTTABBgAMyfCMsY4B9iAAAAAElFTkSuQmCC") repeat 0 0}
+.preview-video{position:relative;max-width:100%;height:0;padding-bottom:62.5%;margin-bottom:10px}.preview-video video{position:absolute;width:100%;height:100%;left:0;top:0;background:#000}
+[class*="icon-"]{display:inline-block;width:16px;height:16px;background:url("<?php echo FM_SELF_URL ?>?img=sprites&amp;t=<?php echo $sprites_ver ?>") no-repeat 0 0;vertical-align:bottom}
+.icon-document{background-position:-16px 0}.icon-folder{background-position:-32px 0}
+.icon-folder_add{background-position:-48px 0}.icon-upload{background-position:-64px 0}
+.icon-arrow_up{background-position:-80px 0}.icon-home{background-position:-96px 0}
+.icon-separator{background-position:-112px 0}.icon-cross{background-position:-128px 0}
+.icon-copy{background-position:-144px 0}.icon-apply{background-position:-160px 0}
+.icon-cancel{background-position:-176px 0}.icon-rename{background-position:-192px 0}
+.icon-checkbox{background-position:-208px 0}.icon-checkbox_invert{background-position:-224px 0}
+.icon-checkbox_uncheck{background-position:-240px 0}.icon-download{background-position:-256px 0}
+.icon-goback{background-position:-272px 0}.icon-folder_open{background-position:-288px 0}
+.icon-file_application{background-position:0 -16px}.icon-file_code{background-position:-16px -16px}
+.icon-file_csv{background-position:-32px -16px}.icon-file_excel{background-position:-48px -16px}
+.icon-file_film{background-position:-64px -16px}.icon-file_flash{background-position:-80px -16px}
+.icon-file_font{background-position:-96px -16px}.icon-file_html{background-position:-112px -16px}
+.icon-file_illustrator{background-position:-128px -16px}.icon-file_image{background-position:-144px -16px}
+.icon-file_music{background-position:-160px -16px}.icon-file_outlook{background-position:-176px -16px}
+.icon-file_pdf{background-position:-192px -16px}.icon-file_photoshop{background-position:-208px -16px}
+.icon-file_php{background-position:-224px -16px}.icon-file_playlist{background-position:-240px -16px}
+.icon-file_powerpoint{background-position:-256px -16px}.icon-file_swf{background-position:-272px -16px}
+.icon-file_terminal{background-position:-288px -16px}.icon-file_text{background-position:-304px -16px}
+.icon-file_word{background-position:-320px -16px}.icon-file_zip{background-position:-336px -16px}
+.icon-logout{background-position:-304px 0}.icon-chain{background-position:-320px 0}
+.icon-link_folder{background-position:-352px -16px}.icon-link_file{background-position:-368px -16px}
+.compact-table{border:0;width:auto}.compact-table td,.compact-table th{width:100px;border:0;text-align:center}.compact-table tr:hover td{background-color:#fff}
+.filename{max-width:420px;white-space:nowrap;overflow:hidden;text-overflow:ellipsis}
+.break-word{word-wrap:break-word}
+.logo-header{margin:0 auto;}
+</style>
+<link rel="icon" href="<?php echo FM_SELF_URL ?>?img=favicon" type="image/png">
+<link rel="shortcut icon" href="<?php echo FM_SELF_URL ?>?img=favicon" type="image/png">
+<?php if (isset($_GET['view']) && FM_USE_HIGHLIGHTJS): ?>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.2.0/styles/<?php echo FM_HIGHLIGHTJS_STYLE ?>.min.css">
+<?php endif; ?>
+</head>
+<body>
+<div id="wrapper">
+  <p class="center"><img src="images/logo-grigri-web.png"></p>
+<?php
